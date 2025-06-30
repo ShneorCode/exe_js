5d48d0514df0323305f85b0d5c47bcdf88a6f0bb
ShneorCode <mm4471661@gmail.com>
2025-06-30 09:03:20 +0300

    Delete Fix-the-Code directory

--- Fix-the-Code/Challenge-1/main.js
+++
@@ -1,8 +0,0 @@
-import { greetUser } from "./userSystem.js";
-
-
-greetUser("Alice");
-greetUser("Bob");
-greetUser("Alice"); // Should say: 'Welcome back, Alice!';
-
-
--- Fix-the-Code/‏‏‏‏‏‏Challenge-4/tempTracker.js
+++
@@ -1,3 +0,0 @@
-import { getPendingTasks } from "./todo.js";
-
-console.log(getPendingTasks()); // Should only show tasks that are NOT completed
